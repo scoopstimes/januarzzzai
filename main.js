9c75483feb57310e98c6b8b322776128b2ffe3f3
scoopstimes <nugrohoadhi740@gmail.com>
2024-12-11 16:04:57 +0700

    Update main.js

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ let history = [];
 
 // Jawaban spesifik sesuai pertanyaan
 const aiResponses = {
+  "siapa namamu kamu": "Nams saya Januarzzz AI",
   "apakah kamu chatgpt": " Saya bukan ChatGPT, Saya adalah Januarzzz AI, asisten berbasis kecerdasan buatan yang dibangun oleh AdhiNug Innovations, Apa yang kamu ingin ketahui?",
   "kamu Chatgpt bukan": "Halo! Saya bukan ChatGPT, tetapi saya adalah Januarzzz AI, asisten berbasis kecerdasan buatan yang dibangun oleh AdhiNug Innovations, Apa yang kamu ingin ketahui?",
   "kamu chatgpt bukan": "Halo! Saya bukan ChatGPT, tetapi saya adalah Januarzzz AI, asisten berbasis kecerdasan buatan yang dibangun oleh AdhiNug Innovations, Apa yang kamu ingin ketahui?",
